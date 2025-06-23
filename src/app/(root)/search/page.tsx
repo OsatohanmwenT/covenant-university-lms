66641c8c4a539728cf9351d07d99fa7cda688eb6
OsatohanmwenT <osarenkhoeosato45@gmail.com>
2025-06-23 16:09:40 +0100

    feat

--- src/app/(root)/search/page.tsx
+++ src/app/(root)/search/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import SearchPageClient from "@/components/shared/SearchPageClient";
 
 export default function Page() {
   return (
     <div className="px-5 mx-auto max-w-[1440px] sm:px-16">
-      <SearchPageClient />
+      <Suspense fallback={<div className="text-white">Loading search...</div>}>
+        <SearchPageClient />
+      </Suspense>
     </div>
   );
 }
